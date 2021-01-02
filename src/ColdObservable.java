014ecb5e0f29a026db53c6f257a74a07a05326d1
marshalexception <sim-trumm@gmx.de>
2021-01-02 17:36:30 +0100

    Initial commit

---
+++ src/ColdObservable.java
@@ -0,0 +1,20 @@
+import io.reactivex.rxjava3.core.Observable;
+
+public class ColdObservable {
+
+    /**
+     * like a cd - resembles finite datasets
+     */
+    public static void main (String[] args) {
+        Observable<String> source = Observable.just("Alpha", "Beta", "Gamma");
+
+        // first observer
+        source.subscribe(s -> System.out.println("Observer 1: " + s));
+
+        // second observer
+        source.map(String::length)
+                .filter(wordLength -> wordLength >= 5)
+                .subscribe(s -> System.out.println("Observer 2: " + s));
+    }
+
+}
---
+++ src/ObservableChapter.java
@@ -0,0 +1,16 @@
+import io.reactivex.rxjava3.core.Observable;
+
+public class ObservableChapter {
+
+    public static void main (String[] args) {
+        Observable<String> source = Observable.just("Alpha", "Beta", "Gamma");
+        // use lambda expressions instead of creating own Observer, e.g.:
+        // Observer<Integer> myObserver = new Observer<>() {...}
+        source.map(String::length)
+                .filter(wordLength -> wordLength > 4)
+                .subscribe(System.out::println,
+                        Throwable::printStackTrace,
+                        () -> System.out.println("Finished!"));
+    }
+
+}
